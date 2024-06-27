a762abf49086fc5f43b991cb0346bf206a8fd634
SahilKumar221998 <sahilja265@gmail.com>
2024-06-27 11:23:48 +0530

    Interface with all the abstract methods of addressbook

---
+++ IAdressBookService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookWithDictionary
+{
+   public interface IAdressBookService
+    {
+        public Dictionary<char, List<ContactPerson>> ReadCsvFile(string path);
+        void addPerson();
+        void editPerson();
+        void removePerson();
+        void showUserDetails();
+        string userCredentialsCheck(string name);
+
+        void searchOnTheBasisOfCity();
+        void searchOnTheBasisOfState();
+    }
+}
